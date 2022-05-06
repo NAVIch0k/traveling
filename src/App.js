0e1e13c42dea2075ff357c39a2c69a5416decf13
ivan <ivan.safonov.09@mail.ru>
2022-05-06 14:45:59 +0300

    restration and login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 import Registration from "./Components/Registration/Registration";
 import Footer from "./Components/Footer/Footer";
+import {Redirect, Route, Switch} from "react-router-dom";
+import LogIn from "./Components/LogIn/LogIn";
 
 function App() {
-  return (
-    <div>
-        <Registration/>
-        <Footer/>
-    </div>
-  );
+    return (
+        <div className="app">
+            <Switch>
+                <Route exact path={'/'} render={() => <Redirect to={'/registration'}/>}/>
+                <Route path={'/registration'} render={() => <Registration/>}/>
+                <Route path={'/login'} render={() => <LogIn/>}/>
+                <Route path='*' render={() => alert("404")}/>
+            </Switch>
+            <Footer/>
+        </div>
+    )
 }
 
 export default App;
--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Field, reduxForm} from "redux-form";
-import s from './Registration.module.scss'
+import s from './LogIn.module.scss'
 import {Form_control} from "../../Common/Forms_control/Forms_controls";
 import {NavLink} from "react-router-dom";
 
@@ -10,18 +10,16 @@ export const Registration_form = (props) => {
         <div className={s.registration}>
             <header className={s.registration__header}>Loctravel</header>
             <form className={s.registration__form}>
-                <h2 className={s.registration__title}>Регистрация</h2>
-                <Field className={s.registration__input} name={'name'} type={'text'} component={Input} placeholder={'Имя'}/>
-                <Field className={s.registration__input} name={'lastName'} type={'text'} component={Input} placeholder={'Фамилия'}/>
+                <h2 className={s.registration__title}>Авторизация</h2>
                 <Field className={s.registration__input} name={'email'} type={'email'} component={Input} placeholder={'Электропочта'}/>
                 <Field className={s.registration__input} name={'password'} type={'password'} component={Input} placeholder={'Пароль'}/>
-                <button className={s.registration__button}>Зарегистрироваться</button>
-                <NavLink to={'/login'}>Войти</NavLink>
+                <button className={s.registration__button}>Войти</button>
+                <NavLink to={'/registration'}>Зарегистрироваться</NavLink>
             </form>
         </div>
     );
 };
 
-const Registration=reduxForm({form:'registration'})(Registration_form)
+const LogIn=reduxForm({form:'login'})(Registration_form)
 
-export default  Registration
+export default  LogIn
