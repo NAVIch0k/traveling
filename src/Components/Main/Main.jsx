a829dab1315dcc3d6eb904103abf89158b327812
ivan <ivan.safonov.09@mail.ru>
2022-05-09 22:06:26 +0300

    description(50%)

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../Header/Header";
 import NavigationImd from "./NavigationImg/NavigationImd";
+import Description from "./Description/Description";
 
 const Main=()=>{
     return(
         <>
             <Header/>
             <NavigationImd/>
+            <Description/>
         </>
     )
 }
