6504b3a5561f2cc029598b607f54301c943c9e04
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2022-05-20 00:11:53 +0300

    add editProfile adaptive

--- src/Components/EditProfile/form/info/Info.jsx
+++ src/Components/EditProfile/form/info/Info.jsx
@@ -1,29 +1,41 @@
 import React from 'react';
 
-//  COMPONENTS
-import Name from './name/Name';
-import Lastname from './lastname/Lastname';
-import Email from './email/Email';
-import Description from './description/Description';
-import Password from './password/Password';
-import Button from './button/Button';
 //  STYLES
 import s from './Info.module.scss'
 
 const Info = () => {
     return (
         <div className={s.info}>
-            <Name />
+            <div className={s.info__name + ' ' + s.info__block}>
+                <h2 className={s.info__title}>Имя</h2>
+                <input className={s.info__input} type="text" placeholder='Евгений' />
+            </div>
 
-            <Lastname />
+            <div className={s.info__lastName + ' ' + s.info__block}>
+                <h2 className={s.info__title}>Фамилия</h2>
+                <input className={s.info__input} type="text" placeholder='Андреев' />
+            </div>
 
-            <Email />
+            <div className={s.info__email + ' ' + s.info__block}>
+                <h2 className={s.info__title}>Электронная почта</h2>
+                <input className={s.info__input} type="text" placeholder='evgeniy@mail.com' />
+            </div>
 
-            <Description />
+            <div className={s.info__description}>
+                <h2 className={s.info__text}>Описание</h2>
+                <p className={s.info__paragraf}>Привет! Меня зовут Женя, и я помогу тебе по-лучше свой родной город - Москву. Я живу здесь давно и поэтому знаю много об этом городе. Лучшие места, где можно поесть. Места для прогулок и отдыха, где тихо и красиво. И вообще много чего. Поэтому пишите и я покажу вам Москву такой, какой она является для коренного жителя.</p>
+            </div>
 
-            <Password />
+            <div className={s.info__password + ' ' + s.info__block}>
+                <h2 className={s.info__title}>Обновление пароля</h2>
+                <input className={s.info__input} type="password" placeholder='Текущий пароль' />
+                <input className={s.info__input} type="password" placeholder='Новый пароль' />
+            </div>
 
-            <Button />
+            <div className={s.info__button}>
+                <button className={s.info__save}>Сохранить изменения</button>
+                <p className={s.info__cancel}>Отмена</p>
+            </div>
         </div>
     );
 };
